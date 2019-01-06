105ed46140871d56de9499cc14226d83c3c01914
blatank <paddocknote@gmail.com>
2019-01-06 14:27:35 +0000

    isMultipleOfSeventeenを実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 整数が17で割りきれるかどうかを判定する
+ * @param {integer} num 
+ */
+function isMultipleOfSeventeen(num) {
+    return ((num % 17) === 0);
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
